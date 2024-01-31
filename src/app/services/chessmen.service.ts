3150228899f4fc369fe7b1642ff36b6bf64e748f
Bob-development <proinvoke228@gmail.com>
2024-01-31 19:12:07 +0200

    add chosen piece

--- src/app/services/chessmen.service.ts
+++ src/app/services/chessmen.service.ts
@@ -6,18 +6,69 @@ import { chessPiecesBlack, chessPiecesWhite} from "../enums";
 })
 
 export class ChessmenService {
+  public chosenChessPiece: HTMLElement = null
 
   constructor() {}
 
-  getPawnEvent(event: MouseEvent){
-    const target = event.target;
+  public asd(a: HTMLDivElement){
+    console.log(a);
+  }
 
+  public getBlackChessPieceEvent(chessPiece: HTMLImageElement, cellIndex: number){
     return (() => {
-      console.log(target);
+      this.chessPieceClassNameHandler(chessPiece);
+      
+      switch (chessPiece.src) {
+        case 'http://localhost:4200/assets/blackChessPiecesPng/black-pawn.png':
+          console.log('pawn');
+          break;
+
+        case 'http://localhost:4200/assets/blackChessPiecesPng/black-rook.png':
+          console.log('rook');
+          break;
+
+        case 'http://localhost:4200/assets/blackChessPiecesPng/black-knight.png':
+          console.log('knight');
+          break;
+
+        case 'http://localhost:4200/assets/blackChessPiecesPng/black-bishop.png':
+          console.log('bishop');
+          break;
+
+        case 'http://localhost:4200/assets/blackChessPiecesPng/black-queen.png':
+          console.log('queen');
+          break;
+
+        case 'http://localhost:4200/assets/blackChessPiecesPng/black-king.png':
+          console.log('king');
+          break;
+      
+        default:
+          break;
+      }
+      
     })()
   }
 
-  getOrderOfBlackFigures(){
+  public getWhiteChessPieceEvent(chessPiece: HTMLImageElement){
+    return (() => {
+      // chessPiece.parentElement.textContent = ''
+    })()
+  }
+
+  public chessPieceClassNameHandler(chessPiece: HTMLImageElement){
+    if(this.chosenChessPiece){
+      this.chosenChessPiece.className = String(chessPiece.className.split(' ').filter(className => className !== 'chosenPiece'))
+      this.chosenChessPiece = null;
+    }
+
+    if(!chessPiece.className.includes('chosenPiece')){
+      chessPiece.className += ' chosenPiece';
+      this.chosenChessPiece = chessPiece;
+    }
+  }
+
+  public getOrderOfBlackFigures(){
     return [
       chessPiecesBlack.rookURL,
       chessPiecesBlack.knightURL,
@@ -39,7 +90,7 @@ export class ChessmenService {
     ]
   }
 
-  getOrderOfWhiteFigures(){
+  public getOrderOfWhiteFigures(){
     return [
       chessPiecesWhite.pawnURL,
       chessPiecesWhite.pawnURL,
